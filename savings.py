1c4be939d3891d6f379427bb86705974163172e3
Olav Vahtras <vahtras@kth.se>
2021-08-30 08:52:58 +0200

    Initial commit

---
+++ savings.py
@@ -0,0 +1,20 @@
+INTEREST = 0.10
+
+START_AGE = 19
+END_AGE = 27
+RETIREMENT_AGE = 67
+
+amount = 0
+
+for age in range(START_AGE, END_AGE):
+    amount += 6000
+    amount = amount * (1 + INTEREST)
+
+for age in range(END_AGE, RETIREMENT_AGE):
+    amount = amount * (1 + INTEREST)
+
+
+print("Savings at retirement", round(amount, 2))
+    
+
+
