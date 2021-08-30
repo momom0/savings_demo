ab66882bc13ee4416f2e6d3c1bab9bcad89dbc7f
Olav Vahtras <vahtras@kth.se>
2021-08-30 09:03:34 +0200

    Calculation in function

--- savings.py
+++ savings.py
@@ -4,14 +4,18 @@ START_AGE = 19
 END_AGE = 27
 RETIREMENT_AGE = 67
 
-amount = 0
 
-for age in range(START_AGE, END_AGE):
-    amount += 6000
-    amount = amount * (1 + INTEREST)
+def savings_counter():
+    amount = 0
 
-for age in range(END_AGE, RETIREMENT_AGE):
-    amount = amount * (1 + INTEREST)
+    for age in range(START_AGE, END_AGE):
+        amount += 6000
+        amount = amount * (1 + INTEREST)
+
+    for age in range(END_AGE, RETIREMENT_AGE):
+        amount = amount * (1 + INTEREST)
+
+    return amount
 
 
 print("Savings at retirement", round(amount, 2))
