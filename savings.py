abc2eaafa3cb2c89e2945621ea46e362fb45f579
Mo <mohuang@kth.se>
2021-09-08 23:18:27 +0200

    changed the start and end age

--- savings.py
+++ savings.py
@@ -1,7 +1,9 @@
+'''i want to be financial indepent next year, so i will change the start age and ending age, besides i increased the saving amount every year into 9000 sek'''
+
 INTEREST = 0.10
 
-START_AGE = 19
-END_AGE = 27
+START_AGE = 23
+END_AGE = 40
 RETIREMENT_AGE = 67
 
 
@@ -9,7 +11,7 @@ def savings_counter():
     amount = 0
 
     for age in range(START_AGE, END_AGE):
-        amount += 6000
+        amount += 9000
         amount = amount * (1 + INTEREST)
 
     for age in range(END_AGE, RETIREMENT_AGE):
